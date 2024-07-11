65a641945f7253170d13ff931c2b1999a7a3b6bc
chitranshu arya <chitranshu.arya9873@gmail.com>
2024-07-12 02:21:30 +0530

    fourth

--- src/Pages/Joinroom.js
+++ src/Pages/Joinroom.js
@@ -60,7 +60,7 @@ export default function Joinroom() {
                 type="email"
                 onChange={(e)=>setEmail(e.target.value)}
                 required
-                className=" mr-5 min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className=" mr-5 join-email min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 placeholder="Enter Email"
               />
              
